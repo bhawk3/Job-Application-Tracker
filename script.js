01dfb3d9fa410aef04a1f4d7a757f4a3c73587cb
Brendan Hawk <brendanhawk03@aol.com>
2025-07-14 07:26:52 -0400

    Created modal functionality and more structure to the project
    Created the modal functionality, close modal button, added structure for how data will be displayed

--- script.js
+++ script.js
@@ -1,8 +1,35 @@
-const addJobAppBtn = document.getElementById("add-job-application-btn")
+const addJobApplicationBtn = document.getElementById("add-job-application-btn")
 const jobForm = document.getElementById("job-form")
+const addJobBtn = document.getElementById("add-job-btn")
+const editBtn = document.getElementById("edit-btn")
+const deleteBtn = document.getElementById("delete-btn")
+const cancelFormBtn = document.getElementById("cancel-form-btn")
+
+// Toggle the form to add new jobs to the list
+addJobBtn.addEventListener("click", () =>
+jobForm.classList.toggle("hidden")
+)
+
+const jobData = []
+
+// This is the submit btn for the add new jobs form
+addJobApplicationBtn.addEventListener("submit", (e) =>
+e.preventDefault()
+)
+
+// Create the new job form into a modal
 
 
-
-function toggleJobForm() {
+// Add Close Form btn functionality
+cancelFormBtn.addEventListener("click", () => 
     jobForm.classList.toggle("hidden")
-}
+)
+
+// Add Edit btn functionality
+
+
+// Add Delete btn functionality
+
+
+// Create the form data functionality
+
