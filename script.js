6b42952fd69d00dc6b380e5860ed5196ea2e0445
Brendan Hawk <brendanhawk03@aol.com>
2025-07-14 06:21:43 -0400

    Initial Set Up
    Set up the file structures, created the form for adding job applications to the list, and created toggle functionality to display the form only when the button is clicked

---
+++ script.js
@@ -0,0 +1,8 @@
+const addJobAppBtn = document.getElementById("add-job-application-btn")
+const jobForm = document.getElementById("job-form")
+
+
+
+function toggleJobForm() {
+    jobForm.classList.toggle("hidden")
+}
