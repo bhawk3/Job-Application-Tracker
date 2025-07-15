6d521b6c81522abc8608953537d634ecf787fc35
Brendan Hawk <brendanhawk03@aol.com>
2025-07-15 08:32:27 -0400

    Created Job form modal and fixed some other styling

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 const addJobApplicationBtn = document.getElementById("add-job-application-btn")
 const jobForm = document.getElementById("job-form")
+const jobtitleInput = document.getElementById("job-title-input")
+const companyNameInput = document.getElementById("company-name-input")
+const dateAppliedInput = document.getElementById("date-applied-input")
+const activityInput = document.getElementById("activity-input")
 const addJobBtn = document.getElementById("add-job-btn")
 const editBtn = document.getElementById("edit-btn")
 const deleteBtn = document.getElementById("delete-btn")
@@ -15,10 +19,11 @@ const jobData = []
 // This is the submit btn for the add new jobs form
 addJobApplicationBtn.addEventListener("submit", (e) =>
 e.preventDefault()
+
+
+
+// The table data can be input through a template literal by adding a tr
 )
-
-// Create the new job form into a modal
-
 
 // Add Close Form btn functionality
 cancelFormBtn.addEventListener("click", () => 
