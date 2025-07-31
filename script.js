abee5fd838895219f75a8deb8be94013db9431aa
Brendan Hawk <brendanhawk03@aol.com>
2025-07-31 08:44:45 -0400

    Starting to set up localStorage functionality. Also, formatted the code

--- script.js
+++ script.js
@@ -1,66 +1,69 @@
-const addJobApplicationBtn = document.getElementById("add-job-application-btn")
-const jobForm = document.getElementById("job-form")
-const jobtitleInput = document.getElementById("job-title-input")
-const companyNameInput = document.getElementById("company-name-input")
-const dateAppliedInput = document.getElementById("date-applied-input")
-const activityInput = document.getElementById("activity-input")
-const addJobBtn = document.getElementById("add-job-btn")
-const cancelFormBtn = document.getElementById("cancel-form-btn")
-const displayedJobInfo = document.getElementById("displayed-job-info")
+const addJobApplicationBtn = document.getElementById("add-job-application-btn");
+const jobForm = document.getElementById("job-form");
+const jobtitleInput = document.getElementById("job-title-input");
+const companyNameInput = document.getElementById("company-name-input");
+const dateAppliedInput = document.getElementById("date-applied-input");
+const activityInput = document.getElementById("activity-input");
+const addJobBtn = document.getElementById("add-job-btn");
+const cancelFormBtn = document.getElementById("cancel-form-btn");
+const displayedJobInfo = document.getElementById("displayed-job-info");
 
-const jobData = []
-let currentEditIndex = null
+const jobData = [];
+let currentEditIndex = null;
+
+const storedJobs = JSON.parse(localStorage.getItem(jobData));
+if (storedJobs) {
+	jobData.push(...storedJobs);
+	postDataToPage();
+}
 
 // Toggle the form to add new jobs to the list
-addJobBtn.addEventListener("click", () =>
-jobForm.classList.toggle("hidden")
-)
+addJobBtn.addEventListener("click", () => jobForm.classList.toggle("hidden"));
 
 // This is the submit btn for the add new jobs form
 jobForm.addEventListener("submit", (e) => {
-e.preventDefault()
-addOrUpdateJob()
-currentEditIndex = null
-})
+	e.preventDefault();
+	addOrUpdateJob();
+	currentEditIndex = null;
+});
 
 // Check if the form data is empty
 const addOrUpdateJob = () => {
-    if (!jobtitleInput.value.trim()) {
-        alert("Please add a job title")
-        return;
-    }
+	if (!jobtitleInput.value.trim()) {
+		alert("Please add a job title");
+		return;
+	}
 
-    const jobInfo = {
-        id: `${jobtitleInput.value.toLowerCase().split(" ").join("-") }`,
-        jobTitle: jobtitleInput.value,
-        companyName: companyNameInput.value,
-        dateApplied: dateAppliedInput.value,
-        activity: activityInput.value
-    }
+	const jobInfo = {
+		id: `${jobtitleInput.value.toLowerCase().split(" ").join("-")}`,
+		jobTitle: jobtitleInput.value,
+		companyName: companyNameInput.value,
+		dateApplied: dateAppliedInput.value,
+		activity: activityInput.value,
+	};
 
-  if (currentEditIndex === null) {
-    jobData.unshift(jobInfo)
-  } else {
-    jobData[currentEditIndex] = jobInfo
-  }
+	if (currentEditIndex === null) {
+		jobData.unshift(jobInfo);
+	} else {
+		jobData[currentEditIndex] = jobInfo;
+	}
 
-//    jobData.push(jobInfo)
-    console.log(jobData)
-    postDataToPage()
+	//    jobData.push(jobInfo)
+	console.log(jobData);
+	postDataToPage();
 
-
-    // Reset form after adding
-    jobForm.reset()
-    jobForm.classList.add("hidden")
-}
+	localStorage.setItem("jobData", JSON.stringify(jobData));
+	// Reset form after adding
+	jobForm.reset();
+	jobForm.classList.add("hidden");
+};
 
 const postDataToPage = () => {
-//Left off here 07/23
-displayedJobInfo.innerHTML = ""
-  
-jobData.forEach((job, index) => {
-    (displayedJobInfo.innerHTML += 
-      ` <tr>
+	//Left off here 07/23
+	displayedJobInfo.innerHTML = "";
+
+	jobData.forEach((job, index) => {
+		displayedJobInfo.innerHTML += ` <tr>
           <!--<td>${job.id}</td>-->
           <td>${job.jobTitle}</td>
           <td>${job.companyName}</td>
@@ -70,48 +73,42 @@ jobData.forEach((job, index) => {
             <button class="edit-btn" data-index="${index}" >Edit</button>
             <button class="delete-btn" data-index="${index}">Delete</button>
           </td>
-        </tr>`
-    ); 
-  })
-}
+        </tr>`;
+	});
+};
 
 // Add Close Form btn functionality
 cancelFormBtn.addEventListener("click", () => {
-    jobForm.reset()
-   jobForm.classList.add("hidden")
-   currentEditIndex = null;
-})
+	jobForm.reset();
+	jobForm.classList.add("hidden");
+	currentEditIndex = null;
+});
 
 // Add Edit btn functionality
 // Add Delete btn functionality
 displayedJobInfo.addEventListener("click", (e) => {
+	if (e.target.classList.contains("delete-btn")) {
+		const indexToDelete = parseInt(e.target.dataset.index, 10);
+		jobData.splice(indexToDelete, 1); // Remove that one job from the array
+		localStorage.setItem("jobData", JSON.stringify(jobData));
+		postDataToPage();
+	}
 
-  if (e.target.classList.contains('delete-btn')) {
-    const indexToDelete = parseInt(e.target.dataset.index, 10);
-    jobData.splice(indexToDelete, 1);  // Remove that one job from the array
-    postDataToPage();
-}
+	if (e.target.classList.contains("edit-btn")) {
+		jobForm.classList.toggle("hidden");
+		const jobArrIndex = parseInt(e.target.dataset.index, 10);
 
-if (e.target.classList.contains("edit-btn")){
-  jobForm.classList.toggle("hidden")  
-  const jobArrIndex = parseInt(e.target.dataset.index, 10);
+		//const editIndex = parseInt(e.target.dataset.index, 10);
+		if (jobArrIndex !== -1) {
+			updatedJobInfo = jobData[jobArrIndex];
 
-  //const editIndex = parseInt(e.target.dataset.index, 10);
-    if (jobArrIndex !== -1) {
+			jobtitleInput.value = updatedJobInfo.jobTitle;
+			companyNameInput.value = updatedJobInfo.companyName;
+			dateAppliedInput.value = updatedJobInfo.dateApplied;
+			activityInput.value = updatedJobInfo.activity;
 
-      updatedJobInfo = jobData[jobArrIndex]
-
-      jobtitleInput.value = updatedJobInfo.jobTitle
-      companyNameInput.value = updatedJobInfo.companyName
-      dateAppliedInput.value = updatedJobInfo.dateApplied
-      activityInput.value = updatedJobInfo.activity
-
-      currentEditIndex = jobArrIndex
-        addJobApplicationBtn.innerText = "Update Job"
-
-    }
-} 
-
-})
-
-
+			currentEditIndex = jobArrIndex;
+			addJobApplicationBtn.innerText = "Update Job";
+		}
+	}
+});
